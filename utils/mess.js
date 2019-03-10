a2b6aa44d3e36ad29439c31ed22df5c6d963aa38
AHNayeem <ahn.nayeem@gmail.com>
2019-03-10 12:31:07 +0600

    up

--- utils/mess.js
+++ utils/mess.js
@@ -90,4 +90,23 @@ var findThisMonthBazar = (data,callback)=>{
 	})
 }
 
-module.exports = {findAllMember,addMeal,findTodayMeal,findTodayBazar,addBazar,findThisMonthMeal,findThisMonthBazar};
+var getbydate = (data,callback)=>{
+	Meal.findOne(data, function (err,result) {
+		if (err) {
+			console.log(err);
+		} else {
+			callback({msg: 'success',data: result});
+		}
+	});
+}
+
+module.exports = {
+	findAllMember,
+	addMeal,
+	findTodayMeal,
+	findTodayBazar,
+	addBazar,
+	findThisMonthMeal,
+	findThisMonthBazar,
+	getbydate
+};
