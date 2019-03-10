524f6ec347dd26b063271036a2d761da75666201
AHNayeem <ahn.nayeem@gmail.com>
2019-03-10 18:07:27 +0600

    update design

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -30,20 +30,24 @@ var {addMeal,findTodayMeal,findTodayBazar,addBazar,findThisMonthMeal,findThisMon
 /* GET home page. */
 router.get('/', function(req, res) {
   if(req.session.login){
-    var resdata = {
-      title : 'Dashboard',
-      msg : null,
-      userData : {
-        user_name : req.session.user_name,
-        user_id:req.session.user_id,
-        user_email:req.session.user_email,
-        user_img:req.session.user_img,
-        mess_name:req.session.mess_name,
-        mess_id:req.session.mess_id,
-        user_role:((req.session.user_role == 1)? 'Manager':'Member')
+    findAllMember({mess_id:req.session.mess_id},(response)=>{
+      var resdata = {
+        title : 'Dashboard',
+        msg : null,
+        user_data : response.data,
+        _:_,
+        userData : {
+          user_name : req.session.user_name,
+          user_id:req.session.user_id,
+          user_email:req.session.user_email,
+          user_img:req.session.user_img,
+          mess_name:req.session.mess_name,
+          mess_id:req.session.mess_id,
+          user_role:((req.session.user_role == 1)? 'Manager':'Member')
+        }
       }
-    }
-    res.render('pages/dashboard/index', resdata);
+      res.render('pages/dashboard/index', resdata);
+    });
   }else{
     res.redirect('/login');
     
