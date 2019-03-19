c096a107012f71a530614a291bdc8276cf9d16d5
dalimchy <dalimchyjony@gmail.com>
2019-03-19 19:11:23 +0600

    up

--- config/keys.js
+++ config/keys.js
@@ -4,8 +4,13 @@ var ip = require('ip');
 //     secret : "asdfy98wyrwaejfspdrqyrq234kjhrasudf"
 // }
 
+// module.exports = {
+//     mongoURI:'mongodb://dalimchy:A123456789@ds129904.mlab.com:29904/junior_mms',
+//     //mongoURI:'mongodb://172.16.0.54/blog24',
+//     secret : "asdfy98wyrwaejfspdrqyrq234kjhrasudf"
+// }
 module.exports = {
-    mongoURI:'mongodb://dalimchy:A123456789@ds129904.mlab.com:29904/junior_mms',
+    mongoURI:'mongodb:dalimchy:123456789@mongodb-1134-0.cloudclusters.net/mmsMongoDB?authSource=admin',
     //mongoURI:'mongodb://172.16.0.54/blog24',
     secret : "asdfy98wyrwaejfspdrqyrq234kjhrasudf"
 }
