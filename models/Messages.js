0795036cf77c806852e60607cb45a1c3646d92da
dalimchy <dalimchyjony@gmail.com>
2019-03-21 16:46:06 +0600

    update model

---
+++ models/Messages.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+//create schema
+const messagesSchema = new Schema({
+    conversation_id:{
+        type:String,
+        required:true
+    },
+    msg_id:{
+        type:String,
+        required:true
+    },
+    msg_body:{
+        type:String,
+        required:true
+    },
+    attach_img:{
+        type:String,
+        default:null
+    },
+    created_by:{
+        type:String,
+        required:true
+    },
+    has_delivered:{
+        type:Number,
+        default:0
+    },
+    created_at:{
+        type:Date,
+        default:Date.now
+    }
+
+});
+
+
+
+module.exports = mongoose.model('messages',messagesSchema);
