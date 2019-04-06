93292151ccad0344a3dd9ad61b816373aeeb4364
AHNayeem <ahn.nayeem@gmail.com>
2019-04-06 12:32:59 +0600

    up

--- config/keys.js
+++ config/keys.js
@@ -1,14 +1,14 @@
 var ip = require('ip');
-module.exports = {
-    mongoURI:'mongodb://localhost:27017/mms',
-    secret : "asdfy98wyrwaejfspdrqyrq234kjhrasudf"
-}
-
 // module.exports = {
-//     mongoURI:'mongodb://dalimchy:A123456789@ds129904.mlab.com:29904/junior_mms',
-//     //mongoURI:'mongodb://172.16.0.54/blog24',
+//     mongoURI:'mongodb://localhost:27017/mms',
 //     secret : "asdfy98wyrwaejfspdrqyrq234kjhrasudf"
 // }
+
+module.exports = {
+    mongoURI:'mongodb://dalimchy:A123456789@ds129904.mlab.com:29904/junior_mms',
+    //mongoURI:'mongodb://172.16.0.54/blog24',
+    secret : "asdfy98wyrwaejfspdrqyrq234kjhrasudf"
+}
 
 // module.exports = {
 //     //mongoURI:'mongodb://dalimchy:123456789@mongodb-1134-0.cloudclusters.net/mmsMongoDB?authSource=admin',
